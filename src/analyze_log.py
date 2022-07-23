097d8c32dd33ccc1b7fd8e7eb418c4f53522eab9
Elithon Silva <elithonsilva@gmail.com>
2022-07-23 16:34:45 -0300

    validate_file_with_invalid_extension

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,5 @@
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    csv_file = path_to_file.split(".")
+
+    if csv_file[1] != 'csv':
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
