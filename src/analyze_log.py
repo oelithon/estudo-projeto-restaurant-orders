3b865ed3a92d5789596f7a2a5a645b1a2d3d5c6b
Elithon Silva <elithonsilva@gmail.com>
2022-07-23 17:47:29 -0300

    validate_nonexistent_file

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,25 @@
+import csv
+
+
+def most_requested_meal_by_maria(orders_list):
+    maria_orders = dict()
+
+    for name, order in orders_list:
+        if name == 'maria':
+            if order not in maria_orders:
+                maria_orders[order] = 1
+            else:
+                maria_orders[order] += 1
+
+    favorite_plate = max(maria_orders, key=maria_orders.get)
+
+    return favorite_plate
+
+
 def analyze_log(path_to_file):
     csv_file = path_to_file.split(".")
 
     if csv_file[1] != 'csv':
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    
+    raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
